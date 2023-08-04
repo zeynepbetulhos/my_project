651ca607ad6f83103237682e9f4773c22732d6bf
Zeynep Betul Hos <zeynepbtl003@gmail.com>
2023-08-04 11:26:20 +0300

    first commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,6 +5,7 @@ import 'package:my_project/profile_page_2/profile_page_2.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
